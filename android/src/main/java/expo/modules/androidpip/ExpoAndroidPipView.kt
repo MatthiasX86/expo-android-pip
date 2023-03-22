191935c47509d204d3f5a4e55fe7f926c9cf458d
Matthew <28681206+MatthiasX86@users.noreply.github.com>
2023-03-22 13:59:37 -0400

    first commit

--- android/src/main/java/expo/modules/androidpip/ExpoAndroidPipView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.androidpip
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ExpoAndroidPipView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,8 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to ExpoAndroidPip.web.ts
-// and on native platforms to ExpoAndroidPip.ts
-import ExpoAndroidPipModule from './ExpoAndroidPipModule';
-import ExpoAndroidPipView from './ExpoAndroidPipView';
-import { ChangeEventPayload, ExpoAndroidPipViewProps } from './ExpoAndroidPip.types';
+import ExpoAndroidPipModule from "./ExpoAndroidPipModule";
 
 // Get the native constant value.
 export const PI = ExpoAndroidPipModule.PI;
 
 export function hello(): string {
-  return ExpoAndroidPipModule.hello();
+  return ExpoAndroidPipModule.activate();
 }
-
-export async function setValueAsync(value: string) {
-  return await ExpoAndroidPipModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoAndroidPipModule ?? NativeModulesProxy.ExpoAndroidPip);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoAndroidPipView, ExpoAndroidPipViewProps, ChangeEventPayload };
