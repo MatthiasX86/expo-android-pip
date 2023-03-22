0a33baf6f6c00ebd1a81c623259dbdc44b99df6c
Matthew <28681206+MatthiasX86@users.noreply.github.com>
2023-03-22 17:18:29 -0400

    WIP - got something

--- android/src/main/java/expo/modules/androidpip/ExpoAndroidPipModule.kt
+++ android/src/main/java/expo/modules/androidpip/ExpoAndroidPipModule.kt
@@ -1,13 +1,26 @@
 package expo.modules.androidpip
 
+import android.app.Activity
+import expo.modules.core.interfaces.Package
+import expo.modules.core.interfaces.ReactActivityLifecycleListener
+
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ExpoAndroidPipModule : Module() {
+//  override fun createReactActivityLifecycleListeners(activityContext: Context): List<ReactActivityLifecycleListener> {
+//    return listOf(MyLibReactActivityLifecycleListener())
+//  }
   override fun definition() = ModuleDefinition {
     Name("ExpoAndroidPip")
-
     // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("activate") { "Hello world!!!! 👋" }
+    Function("activate") {
+      val activity = appContext.activityProvider?.currentActivity
+      val applicationContext = activity?.applicationContext
+      
+      if (applicationContext != null) {
+        activity.setPictureInPictureParams()
+      }
+    }
   }
 }
